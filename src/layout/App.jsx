99302014b89a9c809fba0e66402289e8e24cb0e2
Mario Silva <mario.silvae@outlook.com>
2020-06-25 18:31:08 -0500

    FEAT: Carousel component

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 import '../assets/styles/App.scss';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 
 const App = () => (
   <div className="App">
     <Header />
     <SearchBar />
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+      </Carousel>
+    </Categories>
   </div>
 );
 
